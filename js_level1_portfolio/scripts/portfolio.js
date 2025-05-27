b851b7e5a966b018c738982ce8d8bf92e73fe0e0
jay <wpdl724@gmail.com>
2025-05-27 14:02:36 +0900

    this속성

--- js_level1_portfolio/scripts/portfolio.js
+++ js_level1_portfolio/scripts/portfolio.js
@@ -23,6 +23,14 @@ thumbNailA[0].addEventListener('click',function(){
     popupShow(); // 팝업 출력함수 호출
     popupBg.children[0].children[0].src =this.children[0].src
 });
+thumbNailA[1].addEventListener('click',function(){
+    console.log(0); // 작동테스트
+    console.log(this); // 이벤트 객체 자동인식하는지 확인하기
+    console.log(this.children[0].src); 
+    console.log(popupBg.children[0].children[0].src); //popupBg의 자식의 자식의 src확인하기
+    popupShow(); // 팝업 출력함수 호출
+    popupBg.children[0].children[0].src =this.children[0].src
+});
 thumbNailA[1].addEventListener('click',popupShow);
 thumbNailA[2].addEventListener('click',popupShow);
 function popupShow(){
