ea75a32eeccb62ebe0c17bf29742b00f8dd3bfe2
jay <wpdl724@gmail.com>
2025-06-04 10:19:30 +0900

    네이버 로그인 조건1 완성

--- js_level2_m_naver_login/scripts/login.js
+++ js_level2_m_naver_login/scripts/login.js
@@ -20,3 +20,32 @@ langSelect.addEventListener('click', () =>{
         openif = false; // 상태값 변경
     }
 })
+//네이버 로그인 에러 메세지 알고리즘
+/* 
+조건1) 아이디와 비밀번호를 모두 입력하지 않고 로그인 클릭시 
+조건1 결과) 아이디 또는 전화번호를 입력해주세요.
+조건2) 아이디를 입력하고 비밀번호를 입력하지 않고 로그인 클릭시 
+조건2 결과) 비밀번호를 입력해주세요.
+조건3) 아이디, 비밀번호 모두 입력하고 로그인 클릭시 정보가 틀렸을때 
+조건3 결과) 아이디, 비밀번호 입력정보가 잘못되었습니다. 다시 입력해주세요. 
+조건4) 조건 3 상황에서 로그인실패가 3회 이상일때 영수증퀴즈와 '아이디(로그인 전화번호, 로그인 전용 아이디), 비밀번호 또는 자동입력방지 문자를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.' 메세지 출력
+*/
+
+//조건1) 아이디와 비밀번호를 모두 입력하지 않고 로그인 클릭시 
+// 변수는 총 4개 만들어야 한다 
+const userId = document.querySelector('input[name=user_id]');
+const userPw = document.querySelector('input[name=user_pw]')
+const errorMsg = document.querySelector('.error_msg');
+const loginBtn = document.querySelector('#login_btn');
+console.log(userId, userPw, errorMsg, loginBtn );
+
+loginBtn.addEventListener('click',()=>{
+    /*if(userId.value == '' && userPw.value == ''){
+        errorMsg.textContent = '아이디 또는 전화번호를 입력해주세요.'//p태그니깐 textContent로 작성한다. 속성값은 value
+    }*/ //userId의 값(value)이 빈문자열과 같다
+
+    // 삼항조건활용 조건1 (리액트 React 할때 많이 쓰이는 조건식이다)
+    // 조건식 ? 조건식 참 결과 : 조건식 거짓결과
+    //조건결과대입 변수 = 조건식 ? 조건식 참결과 : 조건식거짓결과
+    errorMsg.textContent= userId.value == '' && userPw.value == '' ? '아이디 또는 전화번호를 입력해주세요' : '';
+}) 
