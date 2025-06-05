d038dd1608902546c226c434c86832c678a9a657
jay <wpdl724@gmail.com>
2025-06-05 10:16:32 +0900

    네이버로그인 조건완성

--- js_level2_m_naver_login/scripts/login.js
+++ js_level2_m_naver_login/scripts/login.js
@@ -31,6 +31,16 @@ langSelect.addEventListener('click', () =>{
 조건4) 조건 3 상황에서 로그인실패가 3회 이상일때 영수증퀴즈와 '아이디(로그인 전화번호, 로그인 전용 아이디), 비밀번호 또는 자동입력방지 문자를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.' 메세지 출력
 */
 
+//네이버 회원정보 저장 DB 
+const naverUserDB =[{
+    id:'aaa',
+    pw:'a1234',
+},{
+    id:'bbb',
+    pw:'b1234',
+}]
+console.log(naverUserDB);
+
 //조건1) 아이디와 비밀번호를 모두 입력하지 않고 로그인 클릭시 
 // 변수는 총 4개 만들어야 한다 
 const userId = document.querySelector('input[name=user_id]');
@@ -40,12 +50,24 @@ const loginBtn = document.querySelector('#login_btn');
 console.log(userId, userPw, errorMsg, loginBtn );
 
 loginBtn.addEventListener('click',()=>{
-    /*if(userId.value == '' && userPw.value == ''){
-        errorMsg.textContent = '아이디 또는 전화번호를 입력해주세요.'//p태그니깐 textContent로 작성한다. 속성값은 value
-    }*/ //userId의 값(value)이 빈문자열과 같다
-
+    if(userId.value == '' && userPw.value == ''){
+        //아이디와 비밀번호를 모두 입력하지 안했을 때 (참) 실행결과
+        errorMsg.textContent = '아이디 또는 전화번호를 입력해주세요.'//p태그니깐 textContent로 작성한다. 속성값은 value //userId의 값(value)이 빈문자열과 같다
+    }else if(userPw.value == ''){
+        //조건2) 조건1이 거짓이고 조건2의 비밀번호를 입력안했는가 (참)
+        errorMsg.textContent = '비밀번호를 입력해주세요.'
+    }else if(userId.value == ''){
+        //조건 3) 조건 1,2가 모두 거짓일때 아이디를 입력안했는가 (참) 
+        errorMsg.textContent = '아이디 또는 전화번호를 입력해주세요.'
+    }else if(userId.value == naverUserDB[0].id && userPw.value == naverUserDB[0].pw){
+        window.location.href = 'https://naver.com' /* 로그인이 성공되면 네이버 홈페이지로 간다 */
+    }else {
+        //조건3 결과) 아이디, 비밀번호 입력정보가 잘못되었습니다. 다시 입력해주세요. 
+        errorMsg.textContent = '아이디(로그인 전화번호, 로그인 전용 아이디), 비밀번호 또는 자동입력방지 문자를 잘못 입력했습니다. 입력하신 내용을 다시 확인해주세요.';
+    }
+    
     // 삼항조건활용 조건1 (리액트 React 할때 많이 쓰이는 조건식이다)
     // 조건식 ? 조건식 참 결과 : 조건식 거짓결과
     //조건결과대입 변수 = 조건식 ? 조건식 참결과 : 조건식거짓결과
-    errorMsg.textContent= userId.value == '' && userPw.value == '' ? '아이디 또는 전화번호를 입력해주세요' : '';
+    // errorMsg.textContent= userId.value == '' && userPw.value == '' ? '아이디 또는 전화번호를 입력해주세요' : '';
 }) 
