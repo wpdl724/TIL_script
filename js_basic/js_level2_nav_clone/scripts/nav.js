35c04a65c19e30c5a5d9a50777b50d56315bcea6
jay <wpdl724@gmail.com>
2025-06-09 11:11:21 +0900

    JB금융그룹

---
+++ js_basic/js_level2_nav_clone/scripts/nav.js
@@ -0,0 +1,9 @@
+// 데스크탑 메뉴 복제 -> 모바일 메뉴 붙여넣기
+const mNavOpen = document.querySelector('.m_nav_open');
+const gnb = document.querySelector('.gnb');
+const gnbClone = gnb.cloneNode(true);
+console.log(mNavOpen, gnb, gnbClone);
+//클론 복제 붙여넣기
+mNavOpen.appendChild(gnbClone);
+
+
